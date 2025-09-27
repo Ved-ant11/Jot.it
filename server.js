58c69f39134e55cf364b752f8ed6a4d24f98ba1c
Vedant Sharma <svedant210@gmail.com>
2025-09-27 12:25:03 +0530

    server changes

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
-import dotenv from "dotenv";
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const httpServer = createServer();
+const httpRequestHandler = (req, res) => {
+  res.writeHead(200);
+  res.end('Server is running and healthy.');
+};
+
+const httpServer = createServer(httpRequestHandler);
+
 const io = new Server(httpServer, {
   cors: {
     origin: process.env.CLIENT_URL || "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+    methods: ["GET", "POST"]
+  }
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`[CONNECTED] User connected with socket ID: ${socket.id}`);
 
-  socket.on("join-document", (documentId) => {
+  socket.on('join-document', (documentId) => {
     socket.join(documentId);
     console.log(`[JOIN] User ${socket.id} joined room: ${documentId}`);
   });
 
-  socket.on("text-change", (data) => {
-    // Use socket.broadcast.to() to send to everyone in the room *except* the sender
-    socket.broadcast.to(data.documentId).emit("receive-change", data.newText);
-    console.log(
-      `[TEXT-CHANGE] User ${socket.id} in room ${data.documentId} changed text.`
-    );
+  socket.on('text-change', (data) => {
+    socket.broadcast.to(data.documentId).emit('receive-change', data.newText);
+    console.log(`[TEXT-CHANGE] User ${socket.id} in room ${data.documentId} changed text.`);
   });
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     console.log(`[DISCONNECTED] User disconnected: ${socket.id}`);
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => {
-  console.log(`🚀 Socket.IO server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
